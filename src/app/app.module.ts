592720a92798b7702cd63ce0bd1bb60d3eeffaaf
gklsndr <gkl.sundar@gmail.com>
2020-04-29 17:18:19 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClient, Observable, forkJoin ],
   declarations: [ AppComponent, SearchPageComponent ],
   bootstrap:    [ AppComponent ]
 })
